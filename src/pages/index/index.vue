f6428953f8308fec3faae38e4b3cac6f1cf35637
LAPTOP-OLLQOH3M\Rita <lorretta_zmy@163.com>
2025-02-26 23:51:44 +0800

    add feat: 热门推荐与猜你喜欢静态页面 + 首页可视区域滚动

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,36 +1,54 @@
 <template>
   <!-- 导航 -->
   <CustomerNavbar />
-  <!-- 轮播图 -->
-  <myCommonSwiper :pageType="1" />
-  <!-- 分类 -->
-  <CustomerCategory :list="categoryList" />
-
-  hello rita
+  <scroll-view scroll-y style="flex: 1">
+    <!-- 轮播图 -->
+    <myCommonSwiper :pageType="1" />
+    <!-- 分类 -->
+    <CustomerCategory :list="categoryList" />
+    <!-- 热门推荐 -->
+    <CustomerHotPanel :list="HotInfoList" />
+    <!-- 猜你喜欢 -->
+    <myCommonGuess />
+  </scroll-view>
 </template>
 
 <script setup>
 import { ref } from 'vue'
+
+import { onLoad } from '@dcloudio/uni-app'
+import { getHomeCategoryMutliAPI, getHomeHotMutliAPI } from '@/services/home'
+
 import CustomerNavbar from './components/CustomerNavbar.vue'
 import CustomerCategory from './components/CustomerCategory.vue'
-import { onLoad } from '@dcloudio/uni-app'
-import { getHomeCategoryMutliAPI } from '@/services/home'
+import CustomerHotPanel from './components/CustomerHotPanel.vue'
 
 // 获取分类数据
 const categoryList = ref([])
 const fetchCategoryMultiData = async () => {
   const res = await getHomeCategoryMutliAPI()
   categoryList.value = res.result
-  console.log('fetchCategoryMultiData', categoryList.value)
+}
+
+// 热门推荐
+const HotInfoList = ref([])
+const fetchHotInfoData = async () => {
+  const res = await getHomeHotMutliAPI()
+  HotInfoList.value = res.result
+  console.log('fetchHotInfoData', HotInfoList.value)
 }
 
 onLoad(() => {
   fetchCategoryMultiData()
+  fetchHotInfoData()
 })
 </script>
 
 <style lang="scss">
 page {
   background-color: #f7f7f7;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
 }
 </style>
--- src/services/home.js
+++ src/services/home.js
@@ -1,5 +1,6 @@
 import { http } from '@/utils/http.ts'
 
+// 首页轮播图
 export const getHomeBannerAPI = (distributionSite = 1) => {
   return http({
     method: 'GET',
@@ -10,9 +11,18 @@ export const getHomeBannerAPI = (distributionSite = 1) => {
   })
 }
 
+// 首页分类
 export const getHomeCategoryMutliAPI = () => {
   return http({
     method: 'GET',
     url: '/home/category/mutli',
+  })
+}
+
+// 首页热门推荐
+export const getHomeHotMutliAPI = () => {
+  return http({
+    method: 'GET',
+    url: '/home/hot/mutli',
   })
 }
